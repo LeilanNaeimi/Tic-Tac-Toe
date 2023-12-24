0b6ed7d820b96d27ffa953a4b3b68c90034fff53
LeilanNaeimi <leilan.n@gmail.com>
2023-12-25 00:09:55 +0300

    fix end game

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cells = document.querySelectorAll(".cell");
 const tableBody = document.querySelector("table");
+
 let board = "";
 
 const player1 = "X";
 const player2 = "O";
+let cnt = 0;
 
 const winComb = [
   [0, 1, 2],
@@ -19,29 +21,37 @@ const winComb = [
 startGame();
 
 function startGame() {
+  cnt = 0;
   document.querySelector(".endGame").style.display = "none";
+  console.log(document.querySelector(".endGame").style.display);
   //make an array with 9 elements from 0 to 0, and return keys
   //and create other array from it
   board = Array.from(Array(9).keys());
-  console.log(board);
   //remove all x and o from the board
   for (let i = 0; i < cells.length; i++) {
     //empty board
     cells[i].innerHTML = "";
     //remove background color
     cells[i].style.removeProperty("background-color");
-
     cells[i].addEventListener("click", turnclick, false);
   }
 }
 
 function turnclick(e) {
   turn(e.target.id, player1);
-  // board[e.target.id] = player1;
-  // document.getElementById(e.target.id).innerHTML = player1;
 }
 
 function turn(id, player) {
-  board[id] = player;
-  document.getElementById(id).innerHTML = player;
+  cnt++;
+  if (cnt > 9) {
+    console.log(cnt);
+    document.querySelector(".endGame").style.display = "block";
+    document.querySelector(".endGame").innerHTML = "End Game";
+    return;
+  } else {
+    // console.log(cnt);
+    board[id] = player;
+    document.getElementById(id).innerHTML = player;
+    // console.log(id, player);
+  }
 }
